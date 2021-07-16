dd816063e7ef94bae76c87508bf5a0844a93d909
sidharth <sidharth@nagarro.com>
2021-07-16 11:54:40 +0530

    3 sum with non duplicate array

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-let obj = {
-  profile: {
-    name: {
-      firstname: "sidharth",
-      lastname: "rehlan",
-    },
-    hobbies: ["playing"],
-  },
-};
-
-const getValue = (obj, keys) => {
-  let newObj = obj;
-  let keysArr = keys.split(".");
-
-  for (let i = 0; i < keysArr.length; i++) {
-    if (typeof newObj[keysArr[i]] === "undefined") {
-      return -1;
-    } else if (i === keysArr.length - 1) {
-      return newObj[keysArr[i]];
-    } else {
-      newObj = newObj[keysArr[i]];
+const checkDuplicate = (result, entry) => {
+  for (let i = 0; i < result.length; i++) {
+    if (result[i][0] === entry[0] && result[i][1] === entry[1]) {
+      return true;
     }
   }
 };
 
-console.log("before...", obj);
-console.log(getValue(obj, "profile.name"));
-console.log("after...", obj);
+var threeSum = function (nums) {
+  let result = [];
+  let len = nums.length;
+
+  if (len < 3) {
+    return [];
+  }
+  for (let i = 0; i < len - 2; i++) {
+    for (let j = i + 1; j < len - 1; j++) {
+      for (let k = j + 1; k < len; k++) {
+        if (nums[i] + nums[j] + nums[k] === 0) {
+          let arr = [nums[i], nums[j], nums[k]];
+          arr.sort((a, b) => a - b);
+          if (!checkDuplicate(result, arr)) {
+            result.push(arr);
+          }
+        }
+      }
+    }
+  }
+  return result;
+};
+
+let nums = [1, 1, -2, 4, 2, -4, 0];
+console.log(threeSum(nums));
