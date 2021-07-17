1ba30839b8459a61fd4d653bc5fa160131056f9d
sidharth <sidharth@nagarro.com>
2021-07-17 10:12:44 +0530

    toggle

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-let obj = {
-  profile: {
-    name: {
-      firstname: "sidharth",
-      lastname: "rehlan",
-    },
-    hobbies: ["playing"],
-  },
-};
+let input = document.querySelector(".toggle-btn input");
 
-const getValue = (obj, keys) => {
-  let newObj = obj;
-  let keysArr = keys.split(".");
-
-  for (let i = 0; i < keysArr.length; i++) {
-    if (typeof newObj[keysArr[i]] === "undefined") {
-      return -1;
-    } else if (i === keysArr.length - 1) {
-      return newObj[keysArr[i]];
-    } else {
-      newObj = newObj[keysArr[i]];
-    }
-  }
-};
-
-console.log("before...", obj);
-console.log(getValue(obj, "profile.name"));
-console.log("after...", obj);
+input.addEventListener("change", (e) => {
+  console.log(e.target.checked);
+});
